f467389b6d19fbd53306ebe27eb55c80149d33a7
sandytan10 <stan10@stuy.edu>
2018-08-08 13:11:26 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -37,12 +37,12 @@ def deluxe_curfew_checker(time)
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
-  if current_time = 11 
-      return "Time to apparate!"
+  if current_time == 11 
+      return "You're in trouble! Better get back to Hogwarts quick!"
   end
-  if current_time = 11
-    return "You're in trouble! Better get back to Hogwarts quick!"
-  else 
+  if current_time >= 11
+    return 
+  elsif 
     return "You have 2 hour(s) left to keep having fun!"
   end
 end
